7dbf9a07cc629147c9cb96546edd1fb66915d41f
shubhambhanarkar <shubhambhanarkar513@gmail.com>
2021-08-14 17:10:45 +0530

    Container

--- src/components/menu/MenuList.js
+++ src/components/menu/MenuList.js
@@ -7,7 +7,7 @@ import CartIcon from "./Cart/CartIcon";
 
 import CartContext from "../store/cart-context";
 
-import { Card, Button } from "reactstrap";
+import { Card, Button, Container, Col, Row } from "reactstrap";
 
 const MenuList = (props) => {
   const history = useHistory();
@@ -23,37 +23,48 @@ const MenuList = (props) => {
   };
   return (
     <div>
-      <div style={{ textAlign: "center" }}>
-        <h1 style={{ color: "white" }}>S&#8523;M</h1>
-        <Button
-          type="button"
-          onClick={props.onShow}
-          style={{
-            backgroundColor: "#720D72",
-            position: "relative",
-            left: "4%"
-          }}
-        >
-          <CartIcon />
-          <span style={{ fontWeight: "bold", fontSize: "1.25rem" }}>
-            {numberOfCartItems}
-          </span>
-        </Button>
-        <Button
-          onClick={logoutHandler}
-          style={{
-            backgroundColor: "#720D72",
-            textAlign: "center",
-            width: "7rem",
-            height: "2.5rem",
-            fontWeight: "bold",
-            borderRadius: "1.5rem",
-            float: "right",
-          }}
-        >
-          Logout
-        </Button>
-      </div>
+      <Container>
+        <Row>
+          <Col style={{ textAlign: "center" }}>
+            <h1 style={{ color: "white", position: "relative", right: "4%", fontSize: "4rem" }}>
+              S&#8523;M
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              type="button"
+              onClick={props.onShow}
+              style={{
+                backgroundColor: "#720D72",
+                float: "right",
+              }}
+            >
+              <CartIcon />
+              <span style={{ fontWeight: "bold", fontSize: "1.25rem" }}>
+                {numberOfCartItems}
+              </span>
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              onClick={logoutHandler}
+              style={{
+                backgroundColor: "#720D72",
+                textAlign: "center",
+                width: "7rem",
+                height: "2.5rem",
+                fontWeight: "bold",
+                borderRadius: "1.5rem",
+                float: "right",
+              }}
+            >
+              Logout
+            </Button>
+          </Col>
+        </Row>
+      </Container>
       <div style={{ padding: "2rem" }}>
         <Card
           style={{
